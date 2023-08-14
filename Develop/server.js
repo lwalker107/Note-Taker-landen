02486907457979e39cead629c7b381f140ea4512
lwalker <landen@example.com>
2023-08-14 17:34:43 -0500

    I accidentally just did the whole thing instead of doing periodic commits

---
+++ Develop/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const PORT = 3001;
+const uuid = require('uniqid');
+const db = require('./db/db.json');
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        res.send(data);
+
+    })
+})
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        const notes = JSON.parse(data);
+        const newNotes = {
+            ...req.body,
+            id: uuid()
+        }
+        notes.push(newNotes);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+            res.json(newNotes);
+        })
+    })
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+
+app.listen(PORT, () => {
+    console.log('`App listening at http://localhost:${PORT} 🚀`')
+})
+
