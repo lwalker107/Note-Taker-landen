6d22846b4fc3a61cc36d6cd8bd66b56bf180fb9b
lwalker <landen@example.com>
2023-08-15 06:38:49 -0500

    delete functionality added

--- Develop/server.js
+++ Develop/server.js
@@ -38,6 +38,30 @@ app.post('/api/notes', (req, res) => {
     })
 })
 
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        const notes = JSON.parse(data);
+        const updatedNotes = notes.filter((note) => note.id !== noteId);
+
+        fs.writeFile('./db/db.json', JSON.stringify(updatedNotes), (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+            res.status(204).send();
+        })
+    })
+})
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
